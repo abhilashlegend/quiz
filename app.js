cba9f115f36c758c5b9a662f00bb9f000ac8e1e0
abhilashlegend <abhilash456a@live.com>
2024-03-11 15:27:04 +0530

    Added bootstrap icons package and setup

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap-icons/font')));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
